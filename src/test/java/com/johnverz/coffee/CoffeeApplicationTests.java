ef2897a3c9a7a057c0735dc75d7255a42825d3dc
johnverz22 <johnny.verzola@lorma.edu>
2025-03-13 08:22:21 +0800

    CRUD

---
+++ src/test/java/com/johnverz/coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnverz.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
